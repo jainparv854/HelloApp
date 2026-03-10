f579fea18c062f4ae114c33c1ea7e184fc06fbbe
jainparv854 <jainparv854@gmail.com>
2026-03-10 14:05:59 +0530

    added

---
+++ uc2.java
@@ -0,0 +1,17 @@
+public class uc2 {
+    public static void main(String[] args) {
+        String[] pattern = {
+        "  *****    *****   ******    *****",
+        " *     *  *     *  *     *  *",
+        " *     *  *     *  *     *  *",
+        " *     *  *     *  ******    *****",
+        " *     *  *     *  *              *",
+        " *     *  *     *  *              *",
+        "  *****    *****   *         *****"
+    };
+    
+    for (String line : pattern) {
+        System.out.println(line);
+        }
+    }
+}
